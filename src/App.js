ba1cec3664e58736ea1993da43fac3f300bfc5fb
TechsavvYash <mittal.yash12@hotmail.com>
2022-04-12 21:12:48 +0530

    initial components
    Signed-off-by: TechsavvYash <mittal.yash12@hotmail.com>

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import './App.css';
 import Login from './Components/login';
 import Question from './Components/question';
 import SignUp from './Components/signup';
-import Error from './Components/error';
+// import Error from './Components/error';
 
 function App() {
   return (
     <div className="App">
-      <Login/>
-      <SignUp/>
+      {/* <Login/> */}
+      {/* <SignUp/> */}
       <Question/>
      
-      <Error/>
+      {/* <Error/> */}
     </div>
   );
 }
