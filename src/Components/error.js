8a65ed2e954b4c4179895350d0ee0e2ac1ded853
Shreya Bansal <bansalshreya2803@gmail.com>
2022-04-12 16:46:44 +0530

    error page
    Signed-off-by: Shreya Bansal <bansalshreya2803@gmail.com>

--- src/Components/error.js
+++ src/Components/error.js
@@ -1,11 +1,8 @@
 import React from "react"
-import "./error.css"
 const Error = () => {
     return (
         <>
-          <div className="Error">
-            <h1 className="e404" >404</h1>
-            </div> 
+          
         </>
     )
 }
