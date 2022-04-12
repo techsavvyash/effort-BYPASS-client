bffd614d578e3cb2b45d78d4af625083eaef4203
Shreya Bansal <bansalshreya2803@gmail.com>
2022-04-12 16:41:08 +0530

    Question layout
    Signed-off-by: Shreya Bansal <bansalshreya2803@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 
 import './App.css';
 import Login from './Components/login';
+import Question from './Components/question';
 import SignUp from './Components/signup';
+import Error from './Components/error';
 
 function App() {
   return (
     <div className="App">
       <Login/>
       <SignUp/>
+      <Question/>
+     
+      <Error/>
     </div>
   );
 }
---
+++ src/Components/error.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "./error.css"
+const Error = () => {
+    return (
+        <>
+          <div className="Error">
+            <h1 className="e404" >404</h1>
+            </div> 
+        </>
+    )
+}
+
+export default Error;
